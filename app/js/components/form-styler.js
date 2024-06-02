c8d6c1a7bff132a8ac17cadfe3d0667c0075e1ea
keryaL <lobachevsky.kerya@gmail.com>
2024-06-02 03:39:34 +0300

    Shop page start. Added page heading with breadcrumbs and sort filter

--- app/js/components/form-styler.js
+++ app/js/components/form-styler.js
@@ -2,14 +2,24 @@
 
 $(function () {
 
-    $('.user-nav__callback-select').styler();
+    $('.user-nav__callback-select').styler(); // header phone dropdown
 
-    $('.jq-selectbox__select').on('click', function () {
-        $('.jq-selectbox__trigger-arrow').toggleClass('jq-selectbox__trigger-arrow--active')
+    $('#phone-list .jq-selectbox__select').on('click', function () {
+        $('#phone-list .jq-selectbox__trigger-arrow').toggleClass('jq-selectbox__trigger-arrow--active')
     });
 
-    $('.jq-selectbox li').on('click', function () {
+    $('#phone-list .jq-selectbox li').on('click', function () {
+        $('#phone-list .jq-selectbox__trigger-arrow').removeClass('jq-selectbox__trigger-arrow--active')
+    });
+
+
+    $('.shop-sort__select').styler(); // shop sort dropdown
+
+    $('#shop-sort .jq-selectbox__select').on('click', function () {
+        $('#shop-sort .jq-selectbox__trigger-arrow').toggleClass('jq-selectbox__trigger-arrow--active')
+    });
+
+    $('#shop-sort .jq-selectbox li').on('click', function () {
         $('.jq-selectbox__trigger-arrow').removeClass('jq-selectbox__trigger-arrow--active')
     });
-
 });
